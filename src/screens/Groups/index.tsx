b2d7701ccc22a9b5f17b7c08a278360ef2c02dcf
Gabriel de Oliveira <gabrieloliverdev@outlook.com>
2023-01-27 00:23:26 -0300

    refactor: increment components on screen Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,13 +4,11 @@ import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    "Galera da Rocket",
-    "Galera do Ignite",
-    "Amigos",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -20,7 +18,12 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
