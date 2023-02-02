76ffd490a1fa8498a825f69f5293c82e26a43e52
Gabriel de Oliveira <gabrieloliverdev@outlook.com>
2023-02-02 00:04:10 -0300

    feat: creation of screen NewGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,33 @@
+import { useNavigation } from "@react-navigation/native";
+
+import { Container, Content, Icon } from "./styles";
+
+import { useState } from "react";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+export function NewGroup() {
+  const [group, setGroup] = useState("");
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
+
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
+      </Content>
+    </Container>
+  );
+}
